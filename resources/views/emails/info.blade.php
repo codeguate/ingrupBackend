97412d73c9ef8e713f5d352e65dbbbd502bb16b3
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-10-31 10:55:52 -0600

    [UPDATED] added email template

--- resources/views/emails/info.blade.php
+++ resources/views/emails/info.blade.php
@@ -2,30 +2,30 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Demystifying Email Design</title>
+<title>Ingrup Contacto</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body style="margin: 0; padding: 0;">
 	<table border="0" cellpadding="0" cellspacing="0" width="100%">	
 		<tr>
 			<td style="padding: 10px 0 30px 0;">
-				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #f2f2f2; border-collapse: collapse;">
 					<tr>
-						<td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
-							<img src="http://ingrup.devcodegt.com/assets/images/logo-animado2.gif" alt="Creating Email Magic" width="300" height="230" style="display: block;" />
+						<td align="center" bgcolor="#f2f2f2" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
+							<img src="http://ingrup.devcodegt.com/assets/images/logo-animado2.gif" alt="Creating Email Magic" width="600" height="200" style="display: block;" />
 						</td>
 					</tr>
 					<tr>
-						<td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+						<td bgcolor="#f2f2f2" style="padding: 40px 30px 40px 30px;">
 							<table border="0" cellpadding="0" cellspacing="0" width="100%">
 								<tr>
-									<td style="color: #153643; font-family: Arial, sans-serif; font-size: 24px;">
-										<b>Lorem ipsum dolor sit amet!</b>
+									<td style="color: #44D62C; font-family: Arial, sans-serif; font-size: 24px;">
+										<b>Haz Recibido un Mensaje, Asunto: {{$asunto}}</b>
 									</td>
 								</tr>
 								<tr>
-									<td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+									<td style="padding: 20px 0 30px 0; color: #000000; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+										{{$mensaje}}
 									</td>
 								</tr>
 								<tr>
@@ -35,13 +35,41 @@
 												<td width="260" valign="top">
 													<table border="0" cellpadding="0" cellspacing="0" width="100%">
 														<tr>
-															<td>
-																<img src="images/left.gif" alt="" width="100%" height="140" style="display: block;" />
-															</td>
-														</tr>
-														<tr>
-															<td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
-																Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+                                                            <td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+                                                                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                                                    <tr>
+                                                                        <td>
+                                                                            Nombre:
+                                                                        </td>
+                                                                        <td>
+                                                                            {{$nombre}}
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Telefono:
+                                                                        </td>
+                                                                        <td>
+                                                                            {{$telefono}}
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Pais:
+                                                                        </td>
+                                                                        <td>
+                                                                            {{$pais}}
+                                                                        </td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>
+                                                                            Mercado:
+                                                                        </td>
+                                                                        <td>
+                                                                            {{$marca->nombre}}
+                                                                        </td>
+                                                                    </tr>
+                                                                </table>
 															</td>
 														</tr>
 													</table>
@@ -52,13 +80,7 @@
 												<td width="260" valign="top">
 													<table border="0" cellpadding="0" cellspacing="0" width="100%">
 														<tr>
-															<td>
-																<img src="images/right.gif" alt="" width="100%" height="140" style="display: block;" />
-															</td>
-														</tr>
-														<tr>
 															<td style="padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
-																Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
 															</td>
 														</tr>
 													</table>
@@ -71,25 +93,25 @@
 						</td>
 					</tr>
 					<tr>
-						<td bgcolor="#ee4c50" style="padding: 30px 30px 30px 30px;">
+						<td bgcolor="#44D62C" style="padding: 30px 30px 30px 30px;">
 							<table border="0" cellpadding="0" cellspacing="0" width="100%">
 								<tr>
 									<td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;" width="75%">
-										&reg; Someone, somewhere 2013<br/>
-										<a href="#" style="color: #ffffff;"><font color="#ffffff">Unsubscribe</font></a> to this newsletter instantly
+										&reg; INGRUP, Guatemala 2019<br/>
+										<a href="#" style="color: #ffffff;">
 									</td>
 									<td align="right" width="25%">
 										<table border="0" cellpadding="0" cellspacing="0">
 											<tr>
-												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+												<td style="font-family: Arial, sans-serif; font-size: 1rem; font-weight: bold;">
 													<a href="http://www.twitter.com/" style="color: #ffffff;">
-														<img src="images/tw.gif" alt="Twitter" width="38" height="38" style="display: block;" border="0" />
+														<img src="http://ingrup.devcodegt.com/assets/images/Home/Modulo-1/Instagram.png" alt="Twitter" width="15" height="15" style="display: block;" border="0" />
 													</a>
 												</td>
 												<td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-												<td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+												<td style="font-family: Arial, sans-serif; font-size: 1rem; font-weight: bold;">
 													<a href="http://www.twitter.com/" style="color: #ffffff;">
-														<img src="images/fb.gif" alt="Facebook" width="38" height="38" style="display: block;" border="0" />
+														<img src="http://ingrup.devcodegt.com/assets/images/Home/Modulo-1/Facebook.png" alt="Facebook" width="10" height="15" style="display: block;" border="0" />
 													</a>
 												</td>
 											</tr>
